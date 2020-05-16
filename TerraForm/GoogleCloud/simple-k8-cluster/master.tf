c995c8f1bcdba15b49f2984f087cd5ff613f717a
m l <sapient007@hotmail.com>
2020-05-16 14:35:51 -0400

    include terraform for k8s cluster

---
+++ TerraForm/GoogleCloud/simple-k8-cluster/master.tf
@@ -0,0 +1,37 @@
+provider "google" {
+  project     = "${var.project}"
+  region      = "${var.region}"
+  credentials = "${var.service_account_key}"
+
+  version = "~> 1.20"
+}
+
+terraform {
+  required_version = "< 0.12.0"
+}
+
+module "infra" {
+  source = "../modules/infra"
+
+  project             = "${var.project}"
+  env_name            = "${var.env_name}"
+  region              = "${var.region}"
+  infrastructure_cidr = "${var.infrastructure_cidr}"
+  internetless        = "${var.internetless}"
+}
+
+module "k8s" {
+  source = "../modules/k8s"
+
+  ssh_public_key = "${var.ssh_public_key}"
+  env_name       = "${var.env_name}"
+  network_name   = "${module.infra.network_name}"
+  username       = "ubuntu"
+  network        = "${module.infra.network}"
+  zones          = "${var.zones}"
+  subnet         = "${module.infra.subnet}"
+  common_init_script      = "${var.common_init_script}"
+  master_node_init_script      = "${var.master_node_init_script}"
+  worker_node_init_script      = "${var.worker_node_init_script}"
+  ssh_private_key         = "${var.ssh_private_key}"
+}
---
+++ TerraForm/GoogleCloud/simple-k8-cluster/variables.tf
@@ -0,0 +1,53 @@
+variable "project" {
+  type = "string"
+}
+
+variable "env_name" {
+  type = "string"
+}
+
+variable "region" {
+  type = "string"
+}
+
+variable "internetless" {
+  default = true
+}
+
+variable "service_account_key" {
+  type = "string"
+}
+
+variable "infrastructure_cidr" {
+  type        = "string"
+  description = "cidr for infrastructure subnet"
+  default     = "192.168.0.0/23"
+}
+
+variable "ssh_public_key" {
+  type = "string"
+}
+
+variable "zones" {
+  type = "list"
+}
+
+variable "ssh_private_key" {
+  type = "string"
+}
+
+variable "common_init_script" {
+  description = "Path to the script for initiliazing vms with common set of tools."
+  default     = "" 
+}
+
+variable "master_node_init_script" {
+  description = "Path to the script to stand up a master node"
+  default = ""  
+}
+
+variable "worker_node_init_script" {
+  description = "Path to the script to have worker node join a cluster"
+  default = ""
+}
+
